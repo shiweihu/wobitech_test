03c6079fd0afe5133de09cbc7e66f2aca73381fe
shiwei hu <405927035@qq.com>
2026-03-31 23:43:23 +1100

    add the marker focus function

--- app/components/MapComponent.tsx
+++ app/components/MapComponent.tsx
@@ -4,12 +4,28 @@ import { MapContainer, TileLayer, useMapEvents } from 'react-leaflet';
 import L from 'leaflet';
 import { usePinStore } from '../store/usePinStore';
 import MyMarkerComponent from './MyMarketComponent';
+import { useEffect, useState } from 'react';
 
+import { useMap } from 'react-leaflet';
+
+// move the map center when a pin's popUp is opened
+function RecenterAutomatically({ center }: { center: L.LatLngExpression }) {
+    const map = useMap();
+
+    useEffect(() => {
+        map.flyTo(center, map.getZoom()); // 如果想要丝滑的动画效果
+    }, [center, map]);
+
+    return null;
+}
 
 export default function MapComponent() {
 
+    // set melbourne as default center
+    const defaultCenter: L.LatLngExpression = [-37.8136, 144.9631];
     // get data from Zustand Store
     const pins = usePinStore((state) => state.pins);
+    const [centerPoint, setCenterPoint] = useState<L.LatLngExpression>(defaultCenter);
 
 
     function MapClickHandler() {
@@ -25,10 +41,19 @@ export default function MapComponent() {
         return null;
     }
 
+    useEffect(() => {
+        const focusMarker = pins.find((item) => {
+            return item.popUp
+        })
+        if (focusMarker != undefined) {
+            setCenterPoint([focusMarker.lat, focusMarker.lng]);
+        }
+
+    }, [pins]);
 
 
-    // set melbourne as default center
-    const defaultCenter: L.LatLngExpression = [-37.8136, 144.9631];
+
+
 
     return (
         <MapContainer center={defaultCenter} zoom={13} scrollWheelZoom={true}>
@@ -36,6 +61,8 @@ export default function MapComponent() {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+
+            <RecenterAutomatically center={centerPoint} />
             <MapClickHandler />
 
             {pins.map((pin, index) => (
